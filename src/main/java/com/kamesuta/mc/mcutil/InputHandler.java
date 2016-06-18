c080805eef865feb7f4f241fa8ad8b318564395e
Kamesuta <kamesuta@gmail.com>
2016-06-18 19:20:47 +0900

    ScreenShot Disabled

--- src/main/java/com/kamesuta/mc/mcutil/InputHandler.java
+++ src/main/java/com/kamesuta/mc/mcutil/InputHandler.java
@@ -1,9 +1,5 @@
 package com.kamesuta.mc.mcutil;
 
-import org.lwjgl.input.Keyboard;
-
-import com.kamesuta.mc.mcutil.screenshot.Action;
-
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.InputEvent;
 import net.minecraft.client.settings.KeyBinding;
@@ -11,20 +7,20 @@ import net.minecraft.client.settings.KeyBinding;
 public class InputHandler {
 	public static final InputHandler INSTANCE = new InputHandler();
 
-	private final Action screenshotaction = new Action();
+	//	private final Action screenshotaction = new Action();
 
-	private static final KeyBinding KEY_BINDING_SCREENSHOT = new KeyBinding("mcutilmod.key.screenshot", Keyboard.KEY_O,
-			"mcutil.key.category");
+	//	private static final KeyBinding KEY_BINDING_SCREENSHOT = new KeyBinding("mcutilmod.key.screenshot", Keyboard.KEY_O,
+	//			"mcutil.key.category");
 
-	public static final KeyBinding[] KEY_BINDINGS = new KeyBinding[] { KEY_BINDING_SCREENSHOT };
+	public static final KeyBinding[] KEY_BINDINGS = new KeyBinding[] { /*KEY_BINDING_SCREENSHOT*/ };
 
 	private InputHandler() {
 	}
 
 	@SubscribeEvent
 	public void onKeyInput(final InputEvent event) {
-		if (KEY_BINDING_SCREENSHOT.isPressed()) {
-			this.screenshotaction.screenShot();
-		}
+		//		if (KEY_BINDING_SCREENSHOT.isPressed()) {
+		//			this.screenshotaction.screenShot();
+		//		}
 	}
 }
